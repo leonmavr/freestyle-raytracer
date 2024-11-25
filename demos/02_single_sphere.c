23f644cf28795357795ca33d9a8ebba86de11789
leonmavr <0xleo.git@gmail.com>
2024-11-25 04:17:11 +0100

    Remove redundant code

--- demos/02_single_sphere.c
+++ demos/02_single_sphere.c
@@ -6,22 +6,6 @@
 #include "renderer.h"
 #include <stdio.h>
 
-
-void demo_camera_to_pbuffer() {
-    // map a small square at the center of the camera into the buffer
-    // assuming the camera has already finished the projection
-    float square_side = 100.0;
-    for (float x = -square_side / 2; x <= square_side / 2; x += 1.0) {
-        for (float y = -square_side / 2; y <= square_side / 2; y += 1.0) {
-            vec3f_t proj = { x, y, 0 };
-            vec3i32_t pbuf_coord = cam2pbuffer(proj);
-            pbuffer_write(pbuf_coord.x, pbuf_coord.y, 255, 0, 0);
-        }
-    }
-    pbuffer_save_ppm("output.ppm");
-}
-
-
 int main() {
     camera_init(0, 0, 400, 60, 60);
     lights_init();
